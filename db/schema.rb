aeda03c1e1ec976f968065824dc57574869200e2
raoakif <akifrao@gmail.com>
2022-07-06 18:43:52 +0500

    Add the missing entries(likes counter and post counter) of the Post class

--- db/schema.rb
+++ db/schema.rb
@@ -36,6 +36,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_06_002150) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "text"
+    t.integer "comments_counter"
+    t.integer "likes_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "author_id", null: false
