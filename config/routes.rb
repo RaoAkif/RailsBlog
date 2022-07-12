d258949de3986dab6300358aa0d63223d449ac37
raoakif <akifrao@gmail.com>
2022-07-12 18:09:06 +0500

    Add basic routing, controllers, and views for users and posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  resources :likes
-  resources :comments
-  resources :posts
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "users#index"
+  get '/posts', to: 'posts#index'
+  get '/users/:id', to: 'users#show'
+
 end
