1aff6afc753f7842bb7831e358fba040ca7e29a5
raoakif <akifrao@gmail.com>
2022-07-18 23:29:22 +0500

    Add Spec from Pull Request 4, removed earlier, to re-setup the project in last two pull requests

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @user = User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.', posts_counter: 0)
+    @user.save
+    @post = Post.new(author: @user, title: 'Post 1', text: 'text 1', comments_counter: 0, likes_counter: 0)
+    @post.save
+    @comment = Comment.new(post: @post, author: @user, text: 'text 1')
+    @comment.save
+  end
+
+  describe 'Comment Model Properties' do
+    it 'author should be present' do
+      @comment.author = nil
+      expect(@comment).to_not be_valid
+    end
+
+    it 'post should exist' do
+      @comment.post = nil
+      expect(@comment).to_not be_valid
+    end
+  end
 end
